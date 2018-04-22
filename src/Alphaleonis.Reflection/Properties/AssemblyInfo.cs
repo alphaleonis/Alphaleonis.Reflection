3418f0d26d330ce91e216033eb41821861888a3b
Alphaleonis <alphaleonis@users.noreply.github.com>
2018-04-22 17:52:57 +0200

    Reorganize.

--- src/Alphaleonis.Reflection/Properties/AssemblyInfo.cs
+++ src/Alphaleonis.Reflection/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("fd2a88bb-9e97-4956-8d47-5c7e9a0402ff")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("Tests.Alphaleonis.Reflection.AttributeTable, PublicKey=002400000480000094000000060200000024000052534131000400000100010033b590a45dff32ded24ceeb7e9ecbad4541e77de3e741c09c26ab0d569a9bccd9ac3997761139f0ede66c514377bb4d3aaa72548f535a6171d6a78884da038a711ad11cfab3369d86ee2de84ecc443c80fb606d8a6e8c337b77b87797239fb3e7e5806982380de7eb75bc6bdb08b4f3a405f146597e173c0357a95ecf8c771bd")]
